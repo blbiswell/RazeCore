99f954fca0f0ea43633d4f600d79f2e7a5092ab1
Brian Biswell <drbiswell@mac.com>
2020-08-09 13:44:06 -0700

    Added first networking API

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -10,7 +10,27 @@ import Foundation
 extension RazeCore {
     
     public class Networking {
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create before using any public APIs
+        public class Manager {
+            public init() {}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkingResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { data, response, error in
+                    let result = data.map(NetworkingResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+            
+        }
         
+        public enum NetworkingResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
     
 }
