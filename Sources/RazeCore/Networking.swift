36904973c4db82598cf3df0e15052365cb2c1ada
Brian Biswell <drbiswell@mac.com>
2020-08-09 13:27:16 -0700

    Added Networking class

---
+++ Sources/RazeCore/Networking.swift
@@ -0,0 +1,16 @@
+//
+//  Networking.swift
+//  RazeCore
+//
+//  Created by Brian Biswell on 8/9/20.
+//
+
+import Foundation
+
+extension RazeCore {
+    
+    public class Networking {
+        
+    }
+    
+}
---
+++ Tests/RazeCoreTests/RazeNetworkingTests.swift
@@ -0,0 +1,22 @@
+//
+//  RazeNetworkingTests.swift
+//  RazeCoreTests
+//
+//  Created by Brian Biswell on 8/9/20.
+//
+
+import XCTest
+
+
+final class RazeNetworkingTests: XCTestCase {
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    static var allTests = [
+        ("testExample", testExample)
+    ]
+
+}
