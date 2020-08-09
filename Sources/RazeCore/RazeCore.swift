24453225a33775f545514a2f19c0061664ca0ff1
Brian Biswell <drbiswell@mac.com>
2020-08-09 12:41:50 -0700

    Added secondary color as a static var

--- Sources/RazeCore/RazeCore.swift
+++ Sources/RazeCore/RazeCore.swift
@@ -29,4 +29,10 @@ public class RazeCore {
     public static var razeColor: UIColor {
         return self.colorFromHexString("006736")
     }
+    
+    
+    /// Returns an object of `UIColor` with a secondary pleasing color.
+    public static var secondaryRazeColor: UIColor {
+        return self.colorFromHexString("FCFFFD")
+    }
 }
